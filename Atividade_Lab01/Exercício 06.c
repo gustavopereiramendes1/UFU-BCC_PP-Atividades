b0047c3610055c9a39b50547c5e5ab4140d42f63
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-03-29 20:15:35 -0300

    Rename Exercício 06.c to Atividade_Lab01/Exercício 06.c

--- Atividade_Lab01/Exercício 06.c
+++ Atividade_Lab01/Exercício 06.c
@@ -5,7 +5,7 @@ int main()
 {
     float temperatura,fah;   //cria duas variaveis do tipo flutuante
     printf("Digite a temperatura em grau Celsius: ");   //imprime uma mensagem inicial
-    scanf("%f" , &temperatura); //Lê um valor e armazena na variavel "temperatura"
+    scanf("%f" , &temperatura); //LÃª um valor e armazena na variavel "temperatura"
     fah = temperatura*(9.0/5.0)+32.0;  //converte o valor de temperatura em celsius para fahrenheit
     printf("A temperatura em Fahrenheit eh: %f \n" , fah);  //imprime o resultado
     return 0;
