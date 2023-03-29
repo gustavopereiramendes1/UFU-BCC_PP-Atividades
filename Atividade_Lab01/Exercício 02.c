8bdd45ae8f704b506c40a5166ec5dc664df4ef36
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-03-29 20:14:26 -0300

    Rename Exercício 02.c to Atividade_Lab01/Exercício 02.c

--- Atividade_Lab01/Exercício 02.c
+++ Atividade_Lab01/Exercício 02.c
@@ -4,7 +4,7 @@
 int main()
 {
     float x;  //cria um variavel 'X' do tipo flutuante
-    scanf("%f" , &x); //lê um valor e armazena em 'X'
+    scanf("%f" , &x); //lÃª um valor e armazena em 'X'
     printf("O valor de X eh: %f" , x);  //imprime o valor de 'X'
     return 0;
 }
