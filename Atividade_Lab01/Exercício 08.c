39da5151d999e7b337da60cb057a7e07c4e9416f
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-03-29 20:16:07 -0300

    Rename Exercício 08.c to Atividade_Lab01/Exercício 08.c

--- Atividade_Lab01/Exercício 08.c
+++ Atividade_Lab01/Exercício 08.c
@@ -5,7 +5,7 @@ int main()
 {
     float kelvin, celsius;  //Cria duas variaveis do tipo flutuante
     printf("Digite a temperatura em Kelvin: ");    //imprime uma mensagem inicial obs:ja ta ficando chato comentar tudo
-    scanf("%f" , &kelvin);                          //Lê o valor e armazena na variavel "kelvin"
+    scanf("%f" , &kelvin);                          //LÃª o valor e armazena na variavel "kelvin"
     celsius = kelvin - 273.15;                      //converte de kelvin para celsius e armazena na variavel "celsius"
     printf("A temperatura em Celsius eh: %f \n" , celsius); //imprime a temperatura em celsius
     return 0;
