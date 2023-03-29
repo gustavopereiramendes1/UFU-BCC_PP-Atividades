47ae0d67d6214a213510f20c6bb56b7336f61c55
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-03-29 20:13:37 -0300

    Rename Exercício 01.c to Atividade_Lab01/Exercício 01.c

--- Atividade_Lab01/Exercício 01.c
+++ Atividade_Lab01/Exercício 01.c
@@ -5,7 +5,7 @@ int main()
 {
     int x;   //Cria uma variavel do tipo inteiro
 
-    scanf("%d" , &x);  //lê um valor e armazena na variavel 'X'
+    scanf("%d" , &x);  //lÃª um valor e armazena na variavel 'X'
     printf("O valor de X eh: %d" , x);  //imprime o valor de 'X'
     return 0;
 }
