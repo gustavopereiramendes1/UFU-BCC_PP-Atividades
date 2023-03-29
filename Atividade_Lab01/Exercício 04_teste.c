150842844d93f2dfa9acffac1f89aa22a30d20c8
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-03-29 20:15:09 -0300

    Rename Exercício 04_teste.c to Atividade_Lab01/Exercício 04_teste.c

--- Atividade_Lab01/Exercício 04_teste.c
+++ Atividade_Lab01/Exercício 04_teste.c
@@ -6,7 +6,7 @@ int main()
 {
     float x, quadrado;  //Cria duas variaveis do tipo flutuante
     printf("Digite um numero real: ");
-    scanf("%f" , &x);   //Lê um valor e armazena em 'X'
+    scanf("%f" , &x);   //LÃª um valor e armazena em 'X'
     quadrado = pow(x ,2);   //Calcula o quadrado de 'X' e armazena na varievel 'quadrado', utilizando uma ferramenta de outra bibliteca
     printf("O quadrado desse valor eh: %.2f \n" , quadrado); //Imprime o valor armazenado na variavel 'quadrado'
     return 0;
