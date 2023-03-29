58728f04b242f192f030d3118a55eb6a6ac1971e
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-03-29 20:15:23 -0300

    Rename Exercício 05.c to Atividade_Lab01/Exercício 05.c

--- Atividade_Lab01/Exercício 05.c
+++ Atividade_Lab01/Exercício 05.c
@@ -4,8 +4,8 @@
 int main()
 {
     int x, dividir;    //cria duas variaveis do tipo inteiro
-    printf("Digite um numero real: ");  //mensagem inicial com instrução
-    scanf("%d" , &x);  //Lê um valor e armazena na variavel 'x'
+    printf("Digite um numero real: ");  //mensagem inicial com instruÃ§Ã£o
+    scanf("%d" , &x);  //LÃª um valor e armazena na variavel 'x'
     dividir = x/5;     //divide o valor de x por 5 e armazena na variavel "dividir"
     printf("A quinta parte de %d eh: %d \n" , x, dividir);  //imprime o resultado
     return 0;
