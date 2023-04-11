9c244776c59e7423fcbdad94eab0176bc15e3ee2
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-04-11 20:34:44 -0300

    Rename Exercício 31.c to Atividade_Lab01/Exercício 31.c

--- Atividade_Lab01/Exercício 31.c
+++ Atividade_Lab01/Exercício 31.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d" , &numero);    //le um valor e armazena na variavel "numero"
     antecessor = numero - 1;   //subtrai em 1 o valor de numero e armazena em antecessor
     printf("O antecessor desse numero eh %d e o sucessor eh %d \n" , antecessor, (numero + 1));   /*fiz de duas formas para fixar a aprendizagem
-                                                                                                    uma é fazendo o calculo antes colocando o valor em uma variavel antes de imprimir,
-                                                                                                    e a outra é fazendo o calculo dentro do printf*/
+                                                                                                    uma Ã© fazendo o calculo antes colocando o valor em uma variavel antes de imprimir,
+                                                                                                    e a outra Ã© fazendo o calculo dentro do printf*/
     return 0;
 }
