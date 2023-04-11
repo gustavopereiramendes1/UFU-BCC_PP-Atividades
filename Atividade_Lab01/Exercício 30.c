df4f11d9215eb8d464ad055b88c83d886f12e647
Gustavo P. Mendes <64999442+gustavopereiramendes1@users.noreply.github.com>
2023-04-11 20:34:31 -0300

    Rename Exercício 30.c to Atividade_Lab01/Exercício 30.c

--- Atividade_Lab01/Exercício 30.c
+++ Atividade_Lab01/Exercício 30.c
@@ -7,7 +7,7 @@ int main()
     printf("Digite o valor em reais: ");
     scanf("%f" , &real);
     printf("Digite a cotacao do dolar americano em reais: ");
-    scanf("%f" , &cotacaododolar);      //lê um valor e armazena na variavel "cotacaododolar"
+    scanf("%f" , &cotacaododolar);      //lÃª um valor e armazena na variavel "cotacaododolar"
     dolar = real / cotacaododolar;      //
     printf("O valor em dolar eh: US$ %.2f\n" , dolar);
     return 0;
